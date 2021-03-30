1ddfac153722edfef630a4479cac56f680e9eb3a
WindygoHz <windygo4949@gmail.com>
2021-03-30 20:05:40 +0100

    Some changes

--- commands/hello.js
+++ commands/hello.js
@@ -1,5 +1,6 @@
 module.exports.run = (message) => {
-    message.channel.send('Hey user!')
+    message.channel.send('Hey bro!')
+    message.channel.send('How are you?')
 }
 
 module.exports.config = {
